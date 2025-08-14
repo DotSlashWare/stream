94645e4687fb1aecc4c2925170dd63c1ec5b4a6a
artumont <197291181+artumont@users.noreply.github.com>
2025-08-14 10:05:05 -0600

    refactor(logger): simplify LoggerMiddleware by removing file logging and unnecessary parameters

--- backend/internal/middleware/logger/formatter.go
+++ backend/internal/middleware/logger/formatter.go
@@ -12,8 +12,8 @@ type CustomFormatter struct{}
 func (formatter *CustomFormatter) Format(param gin.LogFormatterParams) string {
 	timestamp := param.TimeStamp.Format("2006/01/02 - 15:04:05")
 
-	// @returns: "[2006/01/02 - 15:04:05] - [status code] | [latency] | [client IP] | [method] [path]"
-	return fmt.Sprintf("[%s] - %d | %v | %s | %s %s\n",
+	// @returns: "[GIN] {2006/01/02 - 15:04:05} - {status code} | {latency} | {client IP} | {method} {path}"
+	return fmt.Sprintf("[GIN] %s - %d | %v | %s | %s %s\n",
 		timestamp,
 		param.StatusCode,
 		param.Latency,
--- backend/internal/middleware/logger/register.go
+++ backend/internal/middleware/logger/register.go
@@ -1,51 +1,32 @@
 package logger
 
 import (
-	"io"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 )
 
 // Custom middleware for logging HTTP requests in a web application.
 type LoggerMiddleware struct {
-	logPath   string
 	Formatter *CustomFormatter // Might need to be accessed in the future so it'll remain public for now
 }
 
 // Factory function to create a new LoggerMiddleware instance.
-func NewLoggerMiddleware(router *gin.Engine, logPath string) *LoggerMiddleware {
+func NewLoggerMiddleware(router *gin.Engine) *LoggerMiddleware {
 	return &LoggerMiddleware{
-		logPath:   logPath,
 		Formatter: &CustomFormatter{},
 	}
 }
 
 // Sets up the logging middleware for the Gin router with rotation.
 func (middleware *LoggerMiddleware) Register(router *gin.Engine) {
-	logPath := middleware.logPath
-
-	writer, err := rotatelogs.New(
-		logPath+"gin.%Y-%m-%d-%H_%M",
-		rotatelogs.WithLinkName(logPath),
-		rotatelogs.WithMaxAge(24*time.Hour),
-		rotatelogs.WithRotationTime(1*time.Hour),
-	)
-	if err != nil {
-		log.Fatalf("Failed to create gin log file writer: %v.", err)
-	}
-	log.Println("Gin Log file writer created successfully.")
-
-	multiWriter := io.MultiWriter(os.Stdout, writer)
 	router.Use(gin.LoggerWithConfig(
 		gin.LoggerConfig{
 			Formatter: middleware.Formatter.Format,
-			Output:    multiWriter,
+			Output:    os.Stdout,
 			SkipPaths: []string{"/health", "/metrics"}, // Skip logging for health and metrics endpoints
 		},
 	))
-	log.Println("Gin Logger middleware registered successfully.")
+	log.Println("Gin Logger middleware registered successfully for stdout output.")
 }
