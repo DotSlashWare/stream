4bb20388007c5ca6963c67a9c2850213343184f7
artumont <197291181+artumont@users.noreply.github.com>
2025-08-14 10:04:17 -0600

    refactor(config): clean up LoadConfig function for improved readability

--- backend/cmd/bootstrap/config.go
+++ backend/cmd/bootstrap/config.go
@@ -11,26 +11,26 @@ import (
 
 func (app *Application) LoadConfig() {
 	log.Println("Loading configuration.")
-    configPath := filepath.Join("/var/config/stream", "backend.config.json")
+	configPath := filepath.Join("/var/config/stream", "backend.config.json")
 
-    if _, err := os.Stat(configPath); os.IsNotExist(err) {
-        log.Printf("Config file not found at %s, creating new config.", configPath)
-        app.Config = config.NewConfig()
-        app.Config.SetupProtocol()
-        return
-    }
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Printf("Config file not found at %s, creating new config.", configPath)
+		app.Config = config.NewConfig()
+		app.Config.SetupProtocol()
+		return
+	}
 
-    fileData, err := os.ReadFile(configPath)
-    if err != nil {
-        log.Fatalf("Failed to read config file: %v.", err)
-        
-    }
+	fileData, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatalf("Failed to read config file: %v.", err)
 
-    var configData config.Config
-    if err := json.Unmarshal(fileData, &configData); err != nil {
-        log.Fatalf("Failed to unmarshal config JSON: %v.", err)
-    }
+	}
 
-    app.Config = &configData
-    log.Printf("Config loaded successfully from %s.", configPath)
+	var configData config.Config
+	if err := json.Unmarshal(fileData, &configData); err != nil {
+		log.Fatalf("Failed to unmarshal config JSON: %v.", err)
+	}
+
+	app.Config = &configData
+	log.Printf("Config loaded successfully from %s.", configPath)
 }
